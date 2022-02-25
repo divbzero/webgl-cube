89fe6eb311dd41e310fa61bb9ef8e9fa1112a6a1
Chris Lei <chris@divbzero.com>
2022-02-25 10:23:23 -0800

    hard coded cube

--- webgl-cube.js
+++ webgl-cube.js
@@ -11,16 +11,44 @@ class WebglCubeElement extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     switch (name) {
       default:
-        this.render()
+        // do nothing
     }
   }
 
   connectedCallback() {
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.appendChild(this.renderer.domElement)
+
+    this.geometry = new THREE.BoxGeometry()
+    this.material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+    this.cube = new THREE.Mesh(this.geometry, this.material)
+
+    this.scene.add(this.cube)
+    this.camera.position.z = 5
+
     this.render()
   }
 
-  render() {
+  disconnectedCallback() {
+    this.scene = null
+    this.camera = null
+    this.renderer = null
+    this.geometry = null
+    this.material = null
+    this.cube = null
     this.innerHTML = ''
+  }
+
+  render() {
+    requestAnimationFrame(this.render.bind(this))
+
+    this.cube.rotation.x += 0.01
+    this.cube.rotation.y += 0.01
+    this.renderer.render(this.scene, this.camera)
   }
 }
 
